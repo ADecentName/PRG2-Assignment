b99b68c086a00b885e86745c1f845c7d4ecf1e10
nateng1 <s10268454@connect.np.edu.sg>
2025-01-16 10:22:06 +0800

    Create Flight.cs

---
+++ Flight.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_Assignment
+{
+    abstract class Flight
+    {
+        public string flightNumber {  get; set; }
+        public string Origin { get; set; }
+        public string destination { get; set; }
+        public DateTime expectedTime { get; set; }
+        public string status { get; set; }
+        public Flight(string flightNumber, string Origin, string destination, DateTime expectedTime, string status)
+        {
+            this.flightNumber = flightNumber;
+            this.Origin = Origin;
+            this.destination = destination;
+            this.expectedTime = expectedTime;
+            this.status = status;
+        }
+        public double CalculateFees()
+        {
+            double fees = 0;
+            return 0.0;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
