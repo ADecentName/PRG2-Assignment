c90f8adfdca3bd929a9537e1aaaf8ea68baf27ed
nateng1 <s10268454@connect.np.edu.sg>
2025-01-26 18:01:24 +0800

    Flight subclasses and Flights.csv feature
    Added subclass of flight (CFFT,DDJB,LWTT,NORM).
    Added Flights.csv feature to read csv file and create flight objects based on data in csv file. Objects are then added to the flights dictionary

--- Flight.cs
+++ Flight.cs
@@ -21,9 +21,61 @@ namespace PRG2_Assignment
             this.expectedTime = expectedTime;
             this.status = status;
         }
-        public double CalculateFees()
+        public abstract double CalculateFees();
+        public override string ToString()
         {
-            double fees = 0;
+            return base.ToString();
+        }
+    }
+    class NORMFlight : Flight
+    {
+        public NORMFlight(string flightNumber, string Origin, string destination, DateTime expectedTime, string status) : base(flightNumber, Origin, destination, expectedTime, status) { }
+        public override double CalculateFees()
+        {
+            return 0.0;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+    class LWTTFlight : Flight
+    {
+        public double requestFee { get; set; }
+        public LWTTFlight(double requestFee, string flightNumber, string Origin, string destination, DateTime expectedTime, string status) : base(flightNumber, Origin, destination, expectedTime, status)
+        {
+            this.requestFee = requestFee;
+        }
+        public override double CalculateFees()
+        {
+            return 0.0;
+        }
+    }
+    class DDJBFlight : Flight
+    {
+        public double requestFee {  get; set; }
+        public DDJBFlight(double requestFee, string flightNumber, string Origin, string destination, DateTime expectedTime, string status) : base(flightNumber, Origin, destination, expectedTime, status)
+        {
+            this.requestFee = requestFee;
+        }
+        public override double CalculateFees()
+        {
+            return 0.0;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+    class CFFTFlight : Flight
+    {
+        public double requestFee { get; set; }
+        public CFFTFlight(double requestFee, string flightNumber, string Origin, string destination, DateTime expectedTime, string status) : base(flightNumber, Origin, destination, expectedTime, status)
+        {
+            this.requestFee = requestFee;
+        }
+        public override double CalculateFees()
+        {
             return 0.0;
         }
         public override string ToString()
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using PRG2_Assignment;
+using System.Runtime.InteropServices;
 
 string filePath = "flights.csv";
 
@@ -9,3 +10,39 @@ Console.WriteLine(header);
 string? line;
 while ((line = sr.ReadLine()) != null)
     Console.WriteLine(line);
+void LoadFlights(string fileName, Dictionary<string, Flight> flights)
+{
+    var lines = File.ReadLines(fileName).Skip(1);
+    foreach(var line in File.ReadLines(fileName))
+    {
+        if (line != null)
+        {
+            var flightdetails = line.Split(',');
+            string flightNum = flightdetails[0];
+            string origin = flightdetails[1];
+            string destination = flightdetails[2];
+            DateTime expectedTime = DateTime.Parse(flightdetails[3]);
+            string status = flightdetails[4];
+            Flight flight = null;
+
+            if (status == null)
+            {
+                flight = new NORMFlight(flightNum, origin, destination, expectedTime, status);
+            }
+            else if (status.ToUpper() == "CFFT")
+            {
+                flight = new CFFTFlight(10.0,flightNum, origin, destination, expectedTime, status); 
+            }
+            else if (status.ToUpper() == "LWTT")
+            {
+                flight = new LWTTFlight(10.0, flightNum, origin, destination, expectedTime, status);  
+            }
+            else if (status.ToUpper() == "DDJB")
+            {
+                flight = new DDJBFlight(10.0, flightNum, origin, destination, expectedTime, status);   
+            }
+
+            flights[flightNum] = flight;
+        }
+    }
+}
